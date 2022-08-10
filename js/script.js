4e903ba7beeed8b64136fa9c8c920054ab0644df
MJPETTITT <105298876+MJPETTITT@users.noreply.github.com>
2022-08-10 14:31:13 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         scrollPercent = (s / (d - c));
 
         /*var position = (scrollPercent * ($(document).width() - $horizontal.width()));*/
-        var position = (500 -scrollPercent * 20 * ($(document).width() - $horizontal1.width()));
+        var position = (500 -scrollPercent * 2 * ($(document).width() - $horizontal1.width()));
         
         $horizontal1.css({
             'left': position
@@ -30,7 +30,7 @@ $(document).ready(function () {
         scrollPercent = (s / (d - c));
 
         /*var position = (scrollPercent * ($(document).width() - $horizontal.width()));*/
-        var position = (scrollPercent * 20 * ($(document).width() - $horizontal2.width()));
+        var position = (19.5 + scrollPercent * 2 * ($(document).width() - $horizontal2.width()));
         
         $horizontal2.css({
             'left': position
@@ -48,5 +48,32 @@ $(document).ready(function () {
   });
 });
 	
+	$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
+		
+	/*var elementHeight = $('.video-container').height();*/
+		var elementTop = document.getElementsByClassName("video-container")[0].offsetTop;
+		opacityValue = 1 - (elementTop - 100 - scrollTop)/elementTop;
+		
+		if (opacityValue > 1) {
+			opacityValue = 2 - opacityValue;
+		}
+       opacityValue = 1000 * (Math.pow(opacityValue, 50));
+		if (opacityValue > 1) {
+			opacityValue = 1;
+		}
+		console.log(opacityValue);
+		
+
+  $('.blackout').css({
+    opacity: opacityValue
+    })
+					 
+$('.video-container').css({
+    opacity: Math.pow(opacityValue, 50)
+    }
+  );
+});
 	
 });
+
